912bc7ba2ea6eb4e7db9e17bcb1a997c75b6ee18
Abdelrahman Hossam <AbdelrahmanIsLearning@gmail.com>
2023-06-12 00:07:07 +0300

    s - url

--- myMLtestapp/urls.py
+++ myMLtestapp/urls.py
@@ -9,12 +9,12 @@ app_name = 'myMLtestapp'
 
 urlpatterns = [
     path('patients_list/', heart_disease_list, name="heart_disease_list"),
-    path('fdetails/<int:pk>', heart_disease_detail, name="heart_disease_detail"),
+    path('fdetails/<int:pk>/', heart_disease_detail, name="heart_disease_detail"),
     path('submit-form/', SubmitForm.as_view(), name="submit_form"),
     # path('submit-form/', submit_form, name="submit_formFBV"),
-    path('fname/<str:name>', heart_disease_name, name="heart_disease_name"),
+    path('fname/<str:name>/', heart_disease_name, name="heart_disease_name"),
     path('fcreate/', heart_disease_create, name="heart_disease_create"),
     # path('Clist/<str:user_id>', HeartDiseaseApi.as_view(), name="heart_disease_c_list"),
-    path('each_patient/<str:user_id>', HeartDiseaseApi.as_view(), name="heart_disease_c_list"),
+    path('each_patient/<str:user_id>/', HeartDiseaseApi.as_view(), name="heart_disease_c_list"),
 
 ]
